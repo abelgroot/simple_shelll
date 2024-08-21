d29aadee8b9bdc398dd0773eedb3a7eac337bb36
abel.mulugeta <abel.mulugeta.dagne@gmail.com>
2024-08-21 23:20:12 +0300

    execute_command.c

--- execute_command.c
+++ execute_command.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * handle_command_not_found - Prints an error message
- * if the command is not found.
+ * handle_command_not_found - Prints an error
+ * message if the command is not found.
  * @cmd: The command that was not found.
  */
 void handle_command_not_found(char *cmd)
@@ -26,6 +26,41 @@ void execute_command_in_child(char *cmd_path, char *argv[])
 }
 
 /**
+ * is_absolute_path - Checks if a given path is absolute.
+ * @path: The path to check.
+ *
+ * Return: 1 if the path is absolute, 0 otherwise.
+ */
+int is_absolute_path(const char *path)
+{
+	return (path[0] == '/');
+}
+
+/**
+ * handle_absolute_path - Handles execution of commands with absolute paths.
+ * @argv: The command and arguments.
+ *
+ * Return: The full path of the command, or NULL if not executable.
+ */
+char *handle_absolute_path(char *argv[])
+{
+	char *cmd_path;
+
+	cmd_path = strdup(argv[0]);
+	if (!cmd_path)
+		return (NULL);
+
+	if (access(cmd_path, X_OK) != 0)
+	{
+		handle_command_not_found(argv[0]);
+		free(cmd_path);
+		return (NULL);
+	}
+
+	return (cmd_path);
+}
+
+/**
  * execute_command - Executes a command line with arguments.
  * @line: The command line to execute.
  *
@@ -33,27 +68,32 @@ void execute_command_in_child(char *cmd_path, char *argv[])
  */
 int execute_command(char *line)
 {
-	char *argv[MAX_ARGS];
-	int i = 0;
+	char *argv[MAX_ARGS], *cmd_path;
+	int i = 0, status;
 	pid_t child_pid;
-	int status;
-	char *cmd_path;
 
 	argv[i] = strtok(line, " ");
 	while (argv[i] != NULL && i < MAX_ARGS - 1)
-	{
-		i++;
-		argv[i] = strtok(NULL, " ");
-	}
+		argv[i++] = strtok(NULL, " ");
 	argv[i] = NULL;
 
 	if (argv[0] == NULL)
-		return (0);  /* Empty command */
-	cmd_path = find_command_in_path(argv[0]);
-	if (cmd_path == NULL)
+		return (0);
+
+	if (is_absolute_path(argv[0]))
 	{
-		handle_command_not_found(argv[0]);
-		return (-1);
+		cmd_path = handle_absolute_path(argv);
+		if (!cmd_path)
+			return (-1);
+	}
+	else
+	{
+		cmd_path = find_command_in_path(argv[0]);
+		if (cmd_path == NULL)
+		{
+			handle_command_not_found(argv[0]);
+			return (-1);
+		}
 	}
 	child_pid = fork();
 	if (child_pid == -1)
@@ -63,13 +103,9 @@ int execute_command(char *line)
 		return (-1);
 	}
 	if (child_pid == 0)
-	{
 		execute_command_in_child(cmd_path, argv);
-	}
 	else
-	{
 		waitpid(child_pid, &status, 0);
-	}
 	free(cmd_path);
 	return (0);
 }
